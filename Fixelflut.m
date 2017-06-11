86cff4a610149b0016e1712958bb13d65a3f09c6
Mimoja <max-schander@freenet.de>
2017-06-11 21:30:35 +0200

    Working multiclient

--- Fixelflut.m
+++ Fixelflut.m
@@ -15,12 +15,57 @@ uint16_t canvas_width=100;
 uint16_t canvas_height=100;
 OFString* sizeString;
 
-void killClient(OFTCPSocket* clientSocket, OFString* fault){
+
+void blameClient(OFTCPSocket* clientSocket, OFString* fault){
 	[of_stdout writeFormat:@"Client send bullshit: %@\n", fault];
 	[clientSocket writeLine: @"Error in format. Disconnecting"];
-	[clientSocket close];
 };
 
+of_stream_async_read_line_block_t  lineHandleBlock = ^bool(OFStream* stream, OFString* line, OFException* exception){
+	if(line != nil){
+		OFTCPSocket* clientSocket = (OFTCPSocket*) stream;
+		if([line hasPrefix: @"PX "]){
+			OFArray* components = [line componentsSeparatedByString: @" "];
+			if([components count] != 4){
+				blameClient(clientSocket, line);
+				return false;
+			}
+			intmax_t x = [[components objectAtIndex: 1] decimalValue];
+			intmax_t y = [[components objectAtIndex: 2] decimalValue];
+			unsigned int r,g,b,a=255;
+			OFString* color = [components objectAtIndex: 3];
+			int length = [color length];
+			intmax_t colorValue;
+			if(length != 6  && length != 8){
+				blameClient(clientSocket, line);
+				return false;
+			}
+			colorValue = [color hexadecimalValue];
+			
+			if(length == 8){
+				r = colorValue >> 24 & 0xff;
+				g = colorValue >> 16 & 0xff;
+				b = colorValue >> 8 & 0xff;
+				a = colorValue >> 0 & 0xff;
+			}else{
+				r = colorValue >> 16 & 0xff;
+				g = colorValue >> 8 & 0xff;
+				b = colorValue >> 0 & 0xff;
+			}
+			[of_stdout writeFormat:@"set %d, %d to 0x%02x 0x%02x 0x%02x 0x%02x\n", x, y, r,g,b,a];
+			//TODO setPixel
+		}
+		else if([line isEqual: @"SIZE"]){
+			[clientSocket writeLine: sizeString];
+		}
+		else{
+			blameClient(clientSocket, line);
+			return false;
+		}
+	}
+	return true;
+};
+        
 of_tcp_socket_async_accept_block_t acceptBlock = ^bool(OFTCPSocket* socket, OFTCPSocket* clientSocket, OFException* exception){
         
     if (exception) {
@@ -31,48 +76,8 @@ of_tcp_socket_async_accept_block_t acceptBlock = ^bool(OFTCPSocket* socket, OFTC
     OFString* clientAddress = [clientSocket remoteAddress];
     [of_stdout writeFormat: @"New connection from %@\n", clientAddress];
 
-	while(true){
-		OFString* line = [clientSocket readLine];
-		if(line != nil){
-			if([line hasPrefix: @"PX "]){
-				OFArray* components = [line componentsSeparatedByString: @" "];
-				if([components count] != 4){
-					killClient(clientSocket, line);
-				}
-				intmax_t x = [[components objectAtIndex: 1] decimalValue];
-				intmax_t y = [[components objectAtIndex: 2] decimalValue];
-				unsigned int r,g,b,a=255;
-				OFString* color = [components objectAtIndex: 3];
-				int length = [color length];
-				intmax_t colorValue;
-				if(length != 6  && length != 8){
-					killClient(clientSocket, line);
-					return true;
-				}
-				colorValue = [color hexadecimalValue];
-				
-				if(length == 8){
-					r = colorValue >> 24 & 0xff;
-					g = colorValue >> 16 & 0xff;
-					b = colorValue >> 8 & 0xff;
-					a = colorValue >> 0 & 0xff;
-				}else{
-					r = colorValue >> 16 & 0xff;
-					g = colorValue >> 8 & 0xff;
-					b = colorValue >> 0 & 0xff;
-				}
-				[of_stdout writeFormat:@"set %d, %d to 0x%02x 0x%02x 0x%02x 0x%02x\n", x, y, r,g,b,a];
-				//TODO setPixel
-			}
-			else if([line isEqual: @"SIZE"]){
-				[clientSocket writeLine: sizeString];
-			}
-			else{
-				killClient(clientSocket, line);
-				return true;
-			}
-		}//TODO detect discon
-	}
+	[clientSocket asyncReadLineWithBlock: lineHandleBlock];
+
     return true;
 };
 
